dc116fa18e7b64fd235ede1c65a2e998fd5cdb1e
Toomeme <jtoom238@gmail.com>
2025-03-20 15:59:24 -0400

    fix pricing

--- services/pricing.js
+++ services/pricing.js
@@ -88,4 +88,41 @@ function isHoliday(date, holidays) {
     });
 }
 
-// ... (other pricing functions - no changes needed here) ...
+// --- Open Play Pricing ---
+function calculateOpenPlayCost(option, quantity = 1) {
+    // quantity is only relevant for the punch card
+    let cost;
+
+    switch (option) {
+        case 'dropin':
+            cost = 15;
+            break;
+        case 'punchcard':
+            cost = 120 * quantity; //  $120 for 10 visits
+            break;
+        case 'membership':
+            cost = 130;
+            break;
+        default:
+            return { error: "Invalid Open Play option." };
+    }
+
+    return { cost: cost, option: option, quantity: quantity };
+}
+
+// --- Birthday Party Pricing ---
+function calculateBirthdayPartyCost(durationHours) {
+    // Currently, only a 2-hour option is specified.  We can expand this.
+    if (durationHours === 2) {
+        return { cost: 275, durationHours: durationHours };
+    } else {
+        return { error: "Invalid birthday party duration." };
+    }
+}
+
+// Export the functions so they can be used in other files
+module.exports = {
+    calculatePlayGroupCost,
+    calculateOpenPlayCost,
+    calculateBirthdayPartyCost,
+};
